6bf62ee021bb1f4ebcef44ddac149aa7089c0f91
Fuad Hasan <hasanmdfuad@hotmail.com>
2020-08-23 13:12:06 +0600

    final commit added work directory

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = [];
+let workItems= [];
 
 app.set("view engine", "ejs");
 
@@ -24,15 +25,37 @@ app.get("/", function(req, res){
     }
 
     let day = today.toLocaleDateString("en-US",options);
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-    let item = req.body.addItem;
-    items.push(item);
-   res.redirect("/");
+    let item = req.body.newItem;
+
+    if(req.body.list === 'Work'){
+        workItems.push(item);
+        res.redirect("/work");
+    }
+    else{
+
+        items.push(item);
+        res.redirect("/");
+    }
+    
 })
 
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+})
+
+app.post("/work", function(req, res){
+    let wItem =req.body.addItem;
+    workItems.push(wItem);
+    res.redirect("/work");
+})
+
+app.get("/about",function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000!!");
